02d89c1790756d20e2c891bc5860139bf5fb08af
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-04-26 23:32:26 -0700

    Implement caesar cipher that takes string and key as arguments

---
+++ caesar-cipher.rb
@@ -0,0 +1,34 @@
+def caesar_cipher(str, key)
+  alphabet = ("a".."z").to_a
+  userString = str.split("")
+  newString = []
+
+  userString.each do |char|
+
+    if alphabet.include?(char.downcase)
+      alphabetIndex = alphabet.index(char.downcase)
+
+      key.times do
+        alphabetIndex += 1
+
+        if alphabetIndex == 26
+          alphabetIndex = 0
+        end
+      end
+
+      newCharacter = alphabet[alphabetIndex]
+
+      if char == char.upcase
+        newString << newCharacter.upcase
+      else
+        newString << newCharacter
+      end
+    else
+      newString << char
+    end
+  end
+
+  puts newString.join
+end
+
+caesar_cipher("What a string!", 5)
