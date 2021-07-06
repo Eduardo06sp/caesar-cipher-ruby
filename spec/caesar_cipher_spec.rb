13f22706c35b7341873601a516b506d2a0b5fb3a
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-05 23:37:05 -0700

    Test string containing end of alphabet

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -6,4 +6,8 @@ describe '#caesar_cipher' do
   it 'returns the encrypted string' do
     expect(caesar_cipher('abc', 3)).to eql('def')
   end
+
+  it 'wraps from z to a' do
+    expect(caesar_cipher('xyz', 5)).to eql('cde')
+  end
 end
