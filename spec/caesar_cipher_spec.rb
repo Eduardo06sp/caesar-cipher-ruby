ac8fe5304c9245cf50b48f9318011575ae8b5fd4
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-05 23:34:44 -0700

    Test string containing start of alphabet

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 require_relative '../caesar-cipher'
+
+describe '#caesar_cipher' do
+  it "returns the encrypted string" do
+    expect(caesar_cipher('abc', 3)).to eql('def')
+  end
+end
