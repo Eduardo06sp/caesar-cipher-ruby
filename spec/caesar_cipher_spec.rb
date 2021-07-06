71a60e4bef9b45b9368ecf6f242eeabd8d372b6c
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-05 23:35:33 -0700

    Use single quotes

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -3,7 +3,7 @@
 require_relative '../caesar-cipher'
 
 describe '#caesar_cipher' do
-  it "returns the encrypted string" do
+  it 'returns the encrypted string' do
     expect(caesar_cipher('abc', 3)).to eql('def')
   end
 end
